718bffbe8e854570d7e0c7bb01e8cd07b3632836
luky <luzion1508@gmail.com>
2025-05-05 21:34:15 +0700

    [update] content config adjust

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,10 +8,13 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
+    category:z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    excerpt: z.string(),
   }),
 });
 
