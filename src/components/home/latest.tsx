d681136d424e699788dd065103604038d84ffff5
prasetya12 <rizalprasetya22@gmail.com>
2025-05-28 20:28:05 +0700

    [fix] category on homepage blog

--- src/components/home/latest.tsx
+++ src/components/home/latest.tsx
@@ -1,4 +1,4 @@
-import { Calendar, Search,X } from 'lucide-react';
+import { Calendar, Search, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Badge } from '@/components/ui/badge';
